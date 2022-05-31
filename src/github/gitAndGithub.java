814e3409c2021c3156a083c018e6d9bdf06474ff
kendalelcii <kendalelcii@gmail.com>
2022-05-31 16:59:45 -0700

    2nd commit

---
+++ src/github/gitAndGithub.java
@@ -0,0 +1,13 @@
+package github;
+
+public class gitAndGithub {
+	public static void main(String[] args) {
+		System.out.println("today we learn gýthub");
+		System.out.println("today is a very good day");
+
+	}
+	
+
+	
+
+}
--- src/gıthub/gıtAndGıthub.java
+++
@@ -1,5 +0,0 @@
-package gýthub;
-
-public class gýtAndGýthub {
-
-}
